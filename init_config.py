b833a87a9be1d75b1d6251e51a62e6a0c6c446f1
sftech2013 <sftech2013@gmail.com>
2016-04-04 20:05:04 +0000

    Update init_config.py

--- init_config.py
+++ init_config.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = 'app/themes/'
 ALLOWED_EXTENSIONS = set(['jpg','png']) 
 APP_CONFIG = {
 	'title': 'Live',
-	'desc': "<p>Créez vos propres live contributifs, gérez l'affichage temps réel de messages SMS et autres tweets sur de multiples supports: Vidéo-projection, TV connectée, Chromecast, appareils Android, et autres objets connectés (Arduino, SparkCore, ...)</p>\n\n<div class=\"well\">\n    <h1>Veuillez patienter, <small>un GM va vous prendre en main...</small></h1>\n    <p>Vous êtes dans une zone d'attente, veuillez patienter, un Gentil Modo va (peut-être) vous prendre en charge et vous rediriger vers un wall valide</p>\n</div>( si vous êtes le Gentil Modo, <a href=\"/admin/\">connectez-vous à votre dashboard</a>. )",
+	'desc': "<p>Créez vos propres live contributifs, gérez l'affichage temps réel de messages SMS et autres tweets sur de multiples supports: Vidéo-projection, TV connectée, Chromecast, appareils Android, et autres objets connectés (Arduino, SparkCore, ...)</p>\n\n<div class=\"well\">\n    <h1>Veuillez patienter, <small>un moderateur va vous prendre en main...</small></h1>\n    <p>Vous êtes dans une zone d'attente, veuillez patienter, un moderateur va (peut-être) vous prendre en charge et vous rediriger vers un live valide</p>\n</div>( si vous êtes le moderateur, <a href=\"/admin/\">connectez-vous à votre dashboard</a>. )",
     'msg_pause': "<p>Le live est en pause ...</p>\n\n<p><strong>Retrouvez-nous dès 12h !</strong></p>\n\n<p>Les messages envoyés pendant cette interruption n'apparaîtront pas, à plus tard !</p>",
     'modal': 'large',
     'bubble_timeout': 8,
