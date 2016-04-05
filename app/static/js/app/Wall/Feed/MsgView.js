c2e31ea1da95baa9792148ac9452bdf17f860b92
sftech2013 <sftech2013@gmail.com>
2016-04-05 10:40:40 +0000

    Update MsgView.js

--- app/static/js/app/Wall/Feed/MsgView.js
+++ app/static/js/app/Wall/Feed/MsgView.js
@@ -1,4 +1,4 @@
-define( [ 'Wall', 'marionette', 'underscore', 'moment', 'models/Item', 'text!Wall/Feed/msg.html'],
+define( [ 'live', 'marionette', 'underscore', 'moment', 'models/Item', 'text!Wall/Feed/msg.html'],
     function( App, Marionette, _, moment, Item, itemTemplate) {
 
         return Marionette.ItemView.extend( {
@@ -13,7 +13,7 @@ define( [ 'Wall', 'marionette', 'underscore', 'moment', 'models/Item', 'text!Wal
             },
 
             initialize: function(options){
-                // passé depuis le WallController, injecté avec ChildViewOptions de la collection
+                // passé depuis le liveController, injecté avec ChildViewOptions de la collection
                 // pas l'impression que cela serve encore
                 this.model.set('current_race_id', options.current_race_id);
                 this.model.set('ctime_str', moment(this.model.get('ctime')).format('HH:mm:ss'));
